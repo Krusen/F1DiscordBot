b7bc039dd4e10c5e16098d87198b436ecd2a7e92
Søren Kruse <krusenen@gmail.com>
2017-09-11 00:22:32 +0200

    Update some flags

--- src/F1DiscordBot/Flags.cs
+++ src/F1DiscordBot/Flags.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace F1DiscordBot
+﻿namespace F1DiscordBot
 {
     public static class Flags
     {
@@ -19,15 +13,18 @@ namespace F1DiscordBot
                 case "belgian": code = "be"; break;
                 case "brazilian": code = "br"; break;
                 case "british": code = "gb"; break;
-                case "candaian": code = "ca"; break;
+                case "canadian": code = "ca"; break;
                 case "danish": code = "dk"; break;
                 case "dutch": code = "nl"; break;
                 case "finnish": code = "fi"; break;
                 case "french": code = "fr"; break;
                 case "german": code = "de"; break;
                 case "indian": code = "in"; break;
+                case "indonesian": code = "id"; break;
                 case "italian": code = "it"; break;
+                case "japanese": code = "jp"; break;
                 case "mexican": code = "mx"; break;
+                case "polish": code = "pl"; break;
                 case "russian": code = "ru"; break;
                 case "spanish": code = "es"; break;
                 case "swedish": code = "se"; break;
